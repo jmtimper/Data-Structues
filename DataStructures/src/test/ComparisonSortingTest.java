c46014e9914d937630dd40fc73017f7a67071ae5
Jeremy Timperio <34565851+jmtimper@users.noreply.github.com>
2017-12-29 17:50:27 -0500

    Tests for ComparisonSorting.java

---
+++ DataStructures/src/test/ComparisonSortingTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import main.ComparisonSorting;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ComparisonSortingTest {
+
+    @Test
+    public void testBubbleSort(){
+        int[] list = {2,4,5,1,6,3,12,32,53,21,34,23,69};
+        ComparisonSorting test = new ComparisonSorting();
+        assertEquals("[1, 2, 3, 4, 5, 6, 12, 21, 23, 32, 34, 53, 69]", Arrays.toString(test.bubbleSort(list)));
+    }
+
+    @Test
+    public void testSelectionSort(){
+        int[] list = {2,4,5,1,6,3,12,32,53,21,34,23,69};
+        ComparisonSorting test = new ComparisonSorting();
+        assertEquals("[1, 2, 3, 4, 5, 6, 12, 21, 23, 32, 34, 53, 69]", Arrays.toString(test.selectionSort(list)));
+    }
+
+    @Test
+    public void testInsertionSort(){
+        int[] list = {2,4,5,1,6,3,12,32,53,21,34,23,69};
+        ComparisonSorting test = new ComparisonSorting();
+        assertEquals("[1, 2, 3, 4, 5, 6, 12, 21, 23, 32, 34, 53, 69]", Arrays.toString(test.insertionSort(list)));
+    }
+}
