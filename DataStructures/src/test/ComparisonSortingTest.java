f2952623e7d3ea7fd1e3608e84386d922b10d729
Jeremy Timperio <34565851+jmtimper@users.noreply.github.com>
2017-12-30 15:19:06 -0500

    Added test for mergesort

--- DataStructures/src/test/ComparisonSortingTest.java
+++ DataStructures/src/test/ComparisonSortingTest.java
@@ -29,4 +29,11 @@ public class ComparisonSortingTest {
         ComparisonSorting test = new ComparisonSorting();
         assertEquals("[1, 2, 3, 4, 5, 6, 12, 21, 23, 32, 34, 53, 69]", Arrays.toString(test.insertionSort(list)));
     }
+
+    @Test
+    public void testMergeSort(){
+        int[] list = {2,4,5,1,6,3,12,32,53,21,34,23,69};
+        ComparisonSorting test = new ComparisonSorting();
+        assertEquals("[1, 2, 3, 4, 5, 6, 12, 21, 23, 32, 34, 53, 69]", Arrays.toString(test.mergeSort(list)));
+    }
 }
